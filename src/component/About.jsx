4af4bb43ab1a44e77e224b5f9c51127d2762b8e2
ss-aa123 <neermas1@hotmail.com>
2023-03-20 17:35:43 -0400

    edit about page

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,7 +1,8 @@
 const About = () => {
   return (
     <section className="section">
-      <h2>About</h2>
+      <h1>About Us</h1>
+      <h3>Welcome to our company! The number 1 stop to try on anything before you buy! From now on, we are going to help you to first love what you buy!</h3>
     </section>
   );
 };
